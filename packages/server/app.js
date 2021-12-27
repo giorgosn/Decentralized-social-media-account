2be8ba618f25f61497dbd7e25a9536798d3ee87d
Ravi Joshi <ravijoshi@Ravi-Joshi.local>
2021-12-27 23:09:19 +0530

    Added fix for req body

--- packages/server/app.js
+++ packages/server/app.js
@@ -2,10 +2,12 @@
  * This is the main entry-point file for the app
  */
 const express = require('express');
+const bodyParser = require('body-parser');
 const routes = require('./routes');
-const router = express.Router();
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 // routes
 app.use('/',routes);
 
