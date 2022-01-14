29559a8776cd7de5bbcf8ec7f30bb4d2aad78498
giorgosn <sdi1400127@di.uoa.gr>
2022-01-14 16:08:17 +0200

    try to add timestamp

--- packages/server/app.js
+++ packages/server/app.js
@@ -8,7 +8,7 @@ const winston = require('winston');
 
 const logConfiguration = {
     'transports': [
-        new winston.transports.Console({'timestamp':true}),
+        new winston.transports.Console({format: combine(colorize(), timestamp(), myFormat),}),
         new winston.transports.File({ filename: 'logs/webhook.log' })
     ]
 };
